6a20042a4af168f8c7c1ffe4589a7b8995a62a48
Madnok <hungviper016@gmail.com>
2022-11-16 14:38:57 +0700

    bai thi

---
+++ 19474781_PhamMinhHung_Grocery/app/config/categories.js
@@ -0,0 +1,22 @@
+// list of coffee categories
+const categories = [
+  {
+    id: 1,
+    name: "Vegetables",
+  },
+  {
+    id: 2,
+    name: "Meals",
+  },
+  { id: 3, name: "Fruits" },
+  {
+    id: 4,
+    name: "Bread",
+  },
+  {
+    id: 5,
+    name: "Egg",
+  },
+];
+
+export default categories;
---
+++ 19474781_PhamMinhHung_Grocery/app/config/groceries.js
@@ -0,0 +1,91 @@
+
+export default [
+  {
+    id: 1,
+    name: "Brocoli",
+    image: require("../../assets/img/brocoloi.jpg"),
+    price: "2.00",
+    description:
+      "Cappuccino is a coffee drink made with espresso and hot milk. It is traditionally prepared with steamed milk, and is traditionally topped with a small amount of foam.",
+    categoryId: 1,
+    rating: 4.5,
+    included: "With Oat milk",
+  },
+  {
+    id: 2,
+    name: "Cabbage",
+    image: require("../../assets/img/cabbage.jpg"),
+    price: "4.01",
+    description:
+      "Espresso is a coffee drink made with hot coffee beans that are roasted and then brewed with hot water, resulting in a strong, full-bodied, and concentrated typical of a cappuccino. It is traditionally prepared with steamed milk, and is traditionally topped with a small amount of foam.",
+    categoryId: 1,
+    rating: 4.5,
+    included: "With Oat milk",
+  },
+  {
+    id: 3,
+    name: "Sandwich",
+    image: require("../../assets/img/sandwich.jpg"),
+    price: "3.05",
+    description:
+      "Latte is a coffee drink made with espresso and steamed milk. It is traditionally prepared with steamed milk, and is traditionally topped with a small amount of foam.",
+    categoryId: 4,
+    rating: 4.5,
+    included: "With Oat milk",
+  },
+  {
+    id: 4,
+    name: "Custy",
+    image: require("../../assets/img/custy.jpg"),
+    price: "3.00",
+    description:
+      "Flat white is a coffee drink made with espresso and steamed milk. It is traditionally prepared with steamed milk, and is traditionally topped with a small amount of foam.",
+    categoryId: 4,
+    rating: 4.5,
+    included: "With Oat milk",
+  },
+  {
+    id: 5,
+    name: "Salad",
+    image: require("../../assets/img/salad.jpg"),
+    price: "4.00",
+    description:
+      "Cappuccino is a coffee drink made with espresso and hot milk. It is traditionally prepared with steamed milk, and is traditionally topped with a small amount of foam.",
+    categoryId: 2,
+    rating: 4.5,
+    included: "With Oat milk",
+  },
+  {
+    id: 6,
+    name: "Steak",
+    image: require("../../assets/img/steak.jpg"),
+    price: "5.00",
+    description:
+      "Espresso is a coffee drink made with hot coffee beans that are roasted and then brewed with hot water, resulting in a strong, full-bodied, and concentrated typical of a cappuccino. It is traditionally prepared with steamed milk, and is traditionally topped with a small amount of foam.",
+    categoryId: 2,
+    rating: 4.5,
+    included: "With Oat milk",
+  },
+  {
+    id: 7,
+    name: "Apple",
+    image: require("../../assets/img/apple.jpg"),
+    price: "2.07",
+    description:
+      "Cappuccino is a coffee drink made with espresso and hot milk. It is traditionally prepared with steamed milk, and is traditionally topped with a small amount of foam.",
+    categoryId: 3,
+    rating: 4.5,
+    included: "With Oat milk",
+  },
+  {
+    id: 8,
+    name: "Pearl",
+    image: require("../../assets/img/pearl.jpg"),
+    price: "1.99",
+    description:
+      "Espresso is a coffee drink made with hot coffee beans that are roasted and then brewed with hot water, resulting in a strong, full-bodied, and concentrated typical of a cappuccino. It is traditionally prepared with steamed milk, and is traditionally topped with a small amount of foam.",
+    categoryId: 3,
+    rating: 4.5,
+    included: "With Oat milk",
+  },
+];
